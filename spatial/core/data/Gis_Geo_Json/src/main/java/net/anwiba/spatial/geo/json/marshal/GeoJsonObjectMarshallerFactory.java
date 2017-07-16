64faf162e6fbcc31e1982e5eb0fca33eadb44a5e
AndreasWBartels <andreas.bartels@anwiba.de>
2017-07-16 22:06:49 +0200

    libraries release version 1.0.48 unstable; thirdparty updates

---
+++ spatial/core/data/Gis_Geo_Json/src/main/java/net/anwiba/spatial/geo/json/marshal/GeoJsonObjectMarshallerFactory.java
@@ -0,0 +1,36 @@
+/*
+ * #%L
+ * anwiba commons core
+ * %%
+ * Copyright (C) 2007 - 2017 Andreas W. Bartels
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+package net.anwiba.spatial.geo.json.marshal;
+
+public class GeoJsonObjectMarshallerFactory {
+
+  private final boolean isPrittyPrintEnabled;
+
+  public GeoJsonObjectMarshallerFactory(final boolean isPrittyPrintEnabled) {
+    this.isPrittyPrintEnabled = isPrittyPrintEnabled;
+  }
+
+  public <T> GeoJsonObjectMarshaller<T> create(final Class<T> clazz) {
+    return new GeoJsonObjectMarshaller<>(clazz, this.isPrittyPrintEnabled);
+  }
+
+}
